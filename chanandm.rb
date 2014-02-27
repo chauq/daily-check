3188d32eb6f009ab69e98b5315ff89ffd3d350d0
chauq <quang.chau@livestation.com>
2014-02-27 12:41:05 +0000

    removed a line of non working find method and made a new screen method so can name files in chanandm

--- chanandm.rb
+++ chanandm.rb
@@ -9,7 +9,8 @@ class Check
 	end
 	
 	def sign_in (site, chan)
-		if site == "http://www."
+	#Will sign into the mobile or the main site since the text buttons are different 
+		if site == "http://www"
 			@session.click_on("Sign in or register")
 			@session.fill_in("Email", {:with =>"test@streams.com"})
 			@session.fill_in("Password", {:with =>"cologi37"})
@@ -19,23 +20,32 @@ class Check
 			@session.fill_in("Email", {:with =>"test@streams.com"})
 			@session.fill_in("Password", {:with =>"cologi37"})
 			@session.click_on("Log in")
-			@session.visit site + "livestation.com/en/channels/" + chan
+			#Have to go back to the channel site on mobile, since it take you back to the channel list page
+			@session.visit site + ".livestation.com/en/channels/" + chan
+		end
+	end
+	
+	def screenshot (site, chan)
+		if site == "http://www"
+			Win32::Screenshot::Take.of(:foreground).write(chan + " www.png")
+			puts "Screenshot of " + chan + " taken on " + site
+		else
+			Win32::Screenshot::Take.of(:foreground).write(chan + " m.png")
+			puts "Screenshot of " + chan + " taken on " + site
 		end
 	end
 	
 	
 	def channel (site, chan)
 		#This opens the channel page
-		@session.visit site + "livestation.com/en/channels/" + chan
+		@session.visit site + ".livestation.com/en/channels/" + chan
 		#Sign in in order to see the Premium channel
 		sign_in site, chan
-		#Check if the channel page is up by seeing if the word "Livestation" is on the page
-		find('#navigation').should have_button('Sign out')
-		if @session.has_content?("Livestation")
+		sleep 10
+		#Check if the channel page is up by seeing if the word "Livestation" or "You May Like" is on the page
+		if @session.has_content?("Livestation") || @session.has_content?("You May Like")
 			puts "Working!"
-			sleep 10
-			Win32::Screenshot::Take.of(:foreground).write(chan + ".png")
-			puts "Screenshot of " + chan + " taken"
+			screenshot site, chan
 			@session.reset!
 		else
 			puts "Something has gone wrong :("
@@ -47,8 +57,9 @@ end
 	
 #The list for the daily checks
 chan = ["cnni", "cnbc", "bbc-world"]
-site = ["http://m.", "http://www."]
+site = ["http://m", "http://www"]
 
+#check both mobile and website
 site.each do |site|
 	chan.each do |chan|
 		puts "Checking " + chan + " on " + site + ":"
@@ -56,4 +67,5 @@ site.each do |site|
 	end
 end
 	
+exit(-1)
 exit(-1)
